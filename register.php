3e6176c73ee2fa712d994e54cb60ee0297da8694
ZinchenkoE <evgeniy.zinchenko@requestum.com>
2017-08-31 17:50:10 +0300

    require

---
+++ register.php
@@ -0,0 +1,175 @@
+<? require 'head.php' ?>
+<? require 'header.php' ?>
+
+<div class="container">
+    <div class="main">
+        <!-- start registration -->
+        <div class="registration">
+            <div class="registration_left">
+                <h2>new user? <span> create an account </span></h2>
+                <!-- [if IE]
+                    < link rel='stylesheet' type='text/css' href='ie.css'/>
+                 [endif] -->
+
+                <!-- [if lt IE 7]>
+                    < link rel='stylesheet' type='text/css' href='ie6.css'/>
+                <! [endif] -->
+                <script>
+                    (function () {
+
+                        // Create input element for testing
+                        var inputs = document.createElement('input');
+
+                        // Create the supports object
+                        var supports = {};
+
+                        supports.autofocus = 'autofocus' in inputs;
+                        supports.required = 'required' in inputs;
+                        supports.placeholder = 'placeholder' in inputs;
+
+                        // Fallback for autofocus attribute
+                        if (!supports.autofocus) {
+
+                        }
+
+                        // Fallback for required attribute
+                        if (!supports.required) {
+
+                        }
+
+                        // Fallback for placeholder attribute
+                        if (!supports.placeholder) {
+
+                        }
+
+                        // Change text inside send button on submit
+                        var send = document.getElementById('register-submit');
+                        if (send) {
+                            send.onclick = function () {
+                                this.innerHTML = '...Sending';
+                            }
+                        }
+
+                    })();
+                </script>
+                <div class="registration_form">
+                    <!-- Form -->
+                    <form id="registration_form" action="contact.php" method="post">
+                        <div>
+                            <label>
+                                <input placeholder="first name:" type="text" tabindex="1" required autofocus>
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                <input placeholder="last name:" type="text" tabindex="2" required autofocus>
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                <input placeholder="email address:" type="email" tabindex="3" required>
+                            </label>
+                        </div>
+                        <div class="sky-form">
+                            <div class="sky_form1">
+                                <ul>
+                                    <li><label class="radio left"><input type="radio" name="radio" checked=""><i></i>Male</label>
+                                    </li>
+                                    <li><label class="radio"><input type="radio" name="radio"><i></i>Female</label></li>
+                                    <div class="clearfix"></div>
+                                </ul>
+                            </div>
+                        </div>
+                        <div>
+                            <label>
+                                <input placeholder="password" type="password" tabindex="4" required>
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                <input placeholder="retype password" type="password" tabindex="4" required>
+                            </label>
+                        </div>
+                        <div>
+                            <input type="submit" value="create an account" id="register-submit">
+                        </div>
+                        <div class="sky-form">
+                            <label class="checkbox"><input type="checkbox" name="checkbox"><i></i>i agree to shoppe.com
+                                &nbsp;<a class="terms" href="#"> terms of service</a> </label>
+                        </div>
+                    </form>
+                    <!-- /Form -->
+                </div>
+            </div>
+            <div class="registration_left">
+                <h2>existing user</h2>
+                <div class="registration_form">
+                    <!-- Form -->
+                    <form id="registration_form" action="contact.php" method="post">
+                        <div>
+                            <label>
+                                <input placeholder="email:" type="email" tabindex="3" required>
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                <input placeholder="password" type="password" tabindex="4" required>
+                            </label>
+                        </div>
+                        <div>
+                            <input type="submit" value="sign in" id="register-submit">
+                        </div>
+                        <div class="forget">
+                            <a href="#">forgot your password</a>
+                        </div>
+                    </form>
+                    <!-- /Form -->
+                </div>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+        <!-- end registration -->
+    </div>
+</div>
+<div class="foot-top">
+    <div class="container">
+        <div class="col-md-6 s-c">
+            <li>
+                <div class="fooll">
+                    <h5>follow us on</h5>
+                </div>
+            </li>
+            <li>
+                <div class="social-ic">
+                    <ul>
+                        <li><a href="#"><i class="facebok"> </i></a></li>
+                        <li><a href="#"><i class="twiter"> </i></a></li>
+                        <li><a href="#"><i class="goog"> </i></a></li>
+                        <li><a href="#"><i class="be"> </i></a></li>
+                        <li><a href="#"><i class="pp"> </i></a></li>
+                        <div class="clearfix"></div>
+                    </ul>
+                </div>
+            </li>
+            <div class="clearfix"></div>
+        </div>
+        <div class="col-md-6 s-c">
+            <div class="stay">
+                <div class="stay-left">
+                    <form>
+                        <input type="text" placeholder="Enter your email to join our newsletter" required="">
+                    </form>
+                </div>
+                <div class="btn-1">
+                    <form>
+                        <input type="submit" value="join">
+                    </form>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+</div>
+
+<? require 'foot.php' ?>
